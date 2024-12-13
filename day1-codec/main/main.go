f2f2cea44bc2959aeb7a6df9472545679036ef17
wang biao <2402785957@qq.com>
2024-12-13 20:43:51 +0800

    my pc 第三次提交

--- day1-codec/main/main.go
+++ day1-codec/main/main.go
@@ -22,23 +22,24 @@ func startServer(add chan string) {
 
 func main() {
 	add := make(chan string)
-	go startServer(add)
+	go startServer(add) // 服务端监听
 
-	// 用户开始连接服务端
+	// 用户开始连接服务端 ---客户端
 	conn, _ := net.Dial("tcp", <-add)
 	defer conn.Close()
 
 	time.Sleep(time.Second)
 
-	_ = json.NewEncoder(conn).Encode(geerpc.DefaultOption)
-	cc := codec.NewGobCodec(conn)
+	_ = json.NewEncoder(conn).Encode(geerpc.DefaultOption) // 先将校验消息编码到conn处 给服务端校验
+	cc := codec.NewGobCodec(conn)                          // --->cc 就是消息编码的接口方法
 
 	for i := 0; i < 5; i++ {
 		h := &codec.Header{
 			ServiceMethod: "Foo.Sum",
 			Seq:           uint64(i),
 		}
-		_ = cc.Write(h, fmt.Sprintf("geerpc  req %d", i))
+		_ = cc.Write(h, fmt.Sprintf("geerpc  req %d", i)) // 向conn 写入数据  先写header  后写入body-->在接口方法内部
+
 		_ = cc.ReadHeader(h)
 		var reply string
 		_ = cc.ReadBody(&reply)
